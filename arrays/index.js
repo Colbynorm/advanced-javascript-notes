a61620ace18f036c954a2ad8fd15ae54ca23e085
colby <colbynorm41@gmail.com>
2021-04-20 11:06:25 -0500

    added to array notes

--- arrays/index.js
+++ arrays/index.js
@@ -38,3 +38,24 @@ console.log([...arr, ...arr2]);
 
 
 console.log(letters.join(' - '));
+
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for(const movement of movements) {
+    if(movement > 0) {
+        console.log(`You deposited ${movement}`);
+    }else {
+        console.log(`You withdrew ${Math.abs(movement)}`);
+    }
+}
+
+console.log('----- FOREACH -----');
+
+movements.forEach(function(movement) {
+    if(movement > 0) {
+        console.log(`You deposited ${movement}`);
+    }else {
+        console.log(`You withdrew ${Math.abs(movement)}`);
+    }
+});
