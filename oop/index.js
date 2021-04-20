364b47a1b56431b17a70f1829562e7284df98955
colby <colbynorm41@gmail.com>
2021-04-20 16:07:06 -0500

    added to notes

--- oop/index.js
+++ oop/index.js
@@ -3,10 +3,17 @@ const Person = function(firstName, birthYear) {
     console.log(this);
     this.firstName = firstName;
     this.birthYear = birthYear;
-}
+
+    //dont do this
+    // this.calcAge = function() {
+    //     console.log(2021 - this.birthYear);
+    // };
+};
 
 const colby = new Person('Colby', 1995);
-console.log(colby);
+const linnea = new Person('Linnea', 1993);
+console.log(colby, linnea);
+console.log(colby instanceof Person);
 
 // 1. new {} is created
 // 2. function is called, 'this' = {}
